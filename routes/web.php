382a2bfa8dfd7ab1de6b06e7f24e5c4d4e021185
GabrielGJU7 <gjacoboulloa@gmail.com>
2021-07-22 17:27:08 -0500

    change

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', 'Welcome@index');
+Route::controller('Welcome');
 
 //Route::get('/{locale}/article/{urlName}', 	[ Articles::class, 		 'index' ] );
 
